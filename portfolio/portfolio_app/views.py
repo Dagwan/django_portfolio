52480980ff4d621388b753d09bf074569a6278f8
Dagwan Pan'an Danladi <dagwanpan@gmail.com>
2024-03-13 17:51:29 +0100

    Adding thank you page for contact, implemented models for contact page, add forms functions, add thank you css, migrations and adjust all pages to have the functionality working properly

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,5 +1,7 @@
 # Import necessary modules
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# Import the ContacForm class from form.py
+from .forms import ContactForm
 
 # Define a view for the home page
 def home(request):
@@ -15,7 +17,20 @@ def projects(request):
 
 # Define a view for the contact page 
 def contact(request):
-    return render(request, 'portfolio_app/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Save the form data to the database
+            form.save()
+            # Redirect to a success page after successful form submission
+            return render(request, 'portfolio_app/thanks.html')
+    else:
+        # If the request method is GET, create a new instance of the ContactForm
+        form = ContactForm()
+    
+     # Render the contact.html template and pass the form object to it
+    return render(request, 'portfolio_app/contact.html', {'form': form})
+
 
 # Define a view for the services page
 def services(request):
